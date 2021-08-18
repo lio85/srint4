1ea33a8127b8d458ad24f9fb10327a0d9c427456
lio85 <lionelprats2021@gmail.com>
2021-08-18 18:00:34 -0300

    commit

---
+++ src/dataBase/config/config.js
@@ -0,0 +1,64 @@
+//require ('dotenv').config();
+
+/*
+DB_USER = 241116
+DB_PASSWORD = alwaysdata2021
+DB_NAME = lionelprats_proyecto_integrador_lio
+DB_HOST = mysql-lionelprats.alwaysdata.net
+DB_PORT = 3306
+*/
+
+module.exports = {
+
+  "development": {
+    "username": "241116",
+    "password": "alwaysdata2021",
+    "database": "lionelprats_testing_proyecto_integrador_lio_testing",
+    "host": "mysql-lionelprats.alwaysdata.net",
+    "dialect": "mysql",
+    "port": 3306
+    
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "241116",
+    "password": "alwaysdata2021",
+    "database": "lionelprats_proyecto_integrador_lio",
+    "host": "mysql-lionelprats.alwaysdata.net",
+    "dialect": "mysql",
+    "port": 3306
+  }
+}
+
+/*
+DB_USER = 241116
+DB_PASSWORD = alwaysdata2021
+DB_NAME = lionelprats_proyecto_integrador_lio
+DB_HOST = mysql-lionelprats.alwaysdata.net
+DB_PORT = 3306
+*/
+
+/* con esta configuracion estaba conectada a mi base local proyecto_integrador_lio
+  "development": {
+  "username": "root",
+  "password": null,
+  "database": "proyecto_integrador_lio",
+  "host": "127.0.0.1",
+  "dialect": "mysql"
+},*/
+
+// con esta configuracion me conecte a mi base productiva en alwaysdata proyecto_integrador_lio (desde localhost 3000)
+/*"development": {
+  "username": "241116",
+  "password": "alwaysdata2021",
+  "database": "lionelprats_proyecto_integrador_lio",
+  "host": "mysql-lionelprats.alwaysdata.net",
+  "dialect": "mysql",
+  "port": 3306
+},*/
