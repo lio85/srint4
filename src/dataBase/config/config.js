995db1882457c59114e4418e3ce96aba60366eca
lio85 <lionelprats2021@gmail.com>
2021-08-23 13:24:39 -0300

    prueba en el config, solo dejo activa la db productiva para testear que pasa con las imagenes

--- src/dataBase/config/config.js
+++ src/dataBase/config/config.js
@@ -33,7 +33,7 @@ module.exports = {
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql",
     //"port": 3306
