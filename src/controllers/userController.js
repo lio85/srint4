8050906cc0436bdfdf6a34a29300cf4d7aa3b0ad
lio85 <lionelprats2021@gmail.com>
2021-09-01 15:35:58 -0300

    mejore la funcionalidad del formulario de edicion de usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -110,75 +110,59 @@ let userController = {
             })       
     },
     updateProfile: function(req,res){
-        let errors = validationResult(req);
-        if(!errors.isEmpty()){
-            db.user.findByPk(req.params.id)
+        db.user.findByPk(req.params.id)
             .then(function(user){
-                return res.render('users/editProfile', {user, mensajeError: errors.array(), old:req.body})
-            })     
-        }
-        else {
-            if(req.files){
-                db.user.findByPk(req.params.id)
-                .then(function(user){
+                let userImages_path= path.join(__dirname+'../../../public/imagenes/userImages/');
+                let errors = validationResult(req);
+                if(!errors.isEmpty()){
+                    return res.render('users/editProfile', {user, mensajeError: errors.array(), old:req.body})    
+                }
+                else if(req.files){
                     if(user.user_image!=""){
-                        console.log("Esta foto se va a eliminar: "+ user.user_image);
-                        let oldImageRoute= path.join(__dirname+'../../../public/imagenes/userImages/'+user.user_image);
-                        console.log("Esta es su ubicacion: "+ oldImageRoute);
-                        fs.unlinkSync(oldImageRoute); 
+                        let imageToDelete_path= userImages_path + user.user_image;
+                        fs.unlinkSync(imageToDelete_path); 
                     }
-                    const objImages= req.files.userImage;
-                    let new_image= Date.now() + path.extname(objImages.name);
-                    objImages.mv(__dirname+'../../../public/imagenes/userImages/'+new_image,(err)=>{
-                    console.log("Se creó la "+ new_image);
-                    if (err) {
-                        // aqui deberia redirigir a la pagina de error
-                        return res.send("Hubo un error");
-                    }
+                    const objNewImage= req.files.userImage;
+                    let imageUser_name= Date.now() + path.extname(objNewImage.name);
+                    objNewImage.mv(userImages_path + imageUser_name,(err)=>{
+                        if (err) {
+                            // aqui deberia redirigir a la pagina de error
+                            return res.send("Hubo un error");
+                        }
+                    });
                     db.user.update({
                         user_name: req.body.user,
                         lastName_user: req.body.lastNameUser,
-                        user_image: new_image             
+                        user_image: imageUser_name             
                     },{
                         where: {id:req.params.id}
                     })
-                    req.session.destroy()
-                    return res.redirect('/users/login'); 
-                    })     
-                });                        
-            }
-            else if (req.body.deleteImage) {
-                db.user.findByPk(req.params.id)
-                    .then(function(user){
-                        console.log("Esta foto se va a eliminar: "+ user.user_image);
-                        let oldImageRoute= path.join(__dirname+'../../../public/imagenes/userImages/'+user.user_image);
-                        console.log("Esta es su ubicacion: "+ oldImageRoute);
-                        fs.unlinkSync(oldImageRoute); 
-                        db.user.update({ 
-                            user_name: req.body.user,
-                            lastName_user: req.body.lastNameUser,
-                            user_image: "",
-                            },
-                            {
-                                where: {id:req.params.id}
-                            })
-                        req.session.destroy()
-                        return res.redirect('/users/login');
-                    })       
-            }
-            else  {
-                db.user.update({ 
-                    user_name: req.body.user,
-                    lastName_user: req.body.lastNameUser,
-                },
-                {
-                    where: {id:req.params.id}
-                })           
+                }    
+                else if (req.body.deleteImage) {
+                    let imageToDelete_path= userImages_path + user.user_image;
+                    fs.unlinkSync(imageToDelete_path); 
+                    db.user.update({ 
+                        user_name: req.body.user,
+                        lastName_user: req.body.lastNameUser,
+                        user_image: "",
+                        },
+                        {
+                            where: {id:req.params.id}
+                        })                            
+                }
+                else  {
+                    db.user.update({ 
+                        user_name: req.body.user,
+                        lastName_user: req.body.lastNameUser,
+                    },
+                    {
+                        where: {id:req.params.id}
+                    })                               
+                }   
                 req.session.destroy()
-                return res.redirect('/users/login');
-            }       
-        }    
-    }    
+                return res.redirect('/users/login');    
+            })       
+    }   
 }
 
 module.exports = userController;
