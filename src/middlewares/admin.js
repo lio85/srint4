7ad0fd38b0925bd33a3a1293bf0e79ac40b0770b
lio85 <lionelprats2021@gmail.com>
2021-09-01 16:21:29 -0300

    arregle el middleware de administrador

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,7 +1,7 @@
 function adminUser(req, res, next) {
-		let logged = req.session.userLogged;
-        if(logged.admin == 0){
-            res.redirect ("/product")
+	let logged = req.session.userLogged;
+	if(!logged || logged.admin == 0){
+		res.redirect ("/product")
 	}
 	next();
 }
