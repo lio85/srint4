af88c7aacaa9286d3668782aa995ac7d42cc2bbe
lio85 <lionelprats2021@gmail.com>
2021-09-01 02:12:45 -0300

    mejoras

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -114,74 +114,58 @@ let userController = {
         let category = db.category.findAll(); 
         Promise.all([product, category])
             .then(function([product, category]){    
-            let errors = validationResult(req);
-            if(!errors.isEmpty()){ 
-                return res.render('products/editProduct', {product, category, mensajeError: errors.mapped(), old: req.body})                
-            }
-            else if(req.files){  
-                let imageProduct;
-                //if(req.files){
-                const objImages= req.files.productImage;
-                imageProduct= Date.now() + path.extname(objImages.name);
-                objImages.mv(__dirname+'../../../public/imagenes/productImages/'+imageProduct,(err)=>{
-                    if (err) {
-                        // aqui deberia redirigir a la pagina de error
-                        return res.send("Hubo un error");
-                    }
-                });
-                db.product.update({
-                    name: req.body.name,
-                    id_category: req.body.category,
-                    description: req.body.description,
-                    stock: req.body.stock,
-                    price: req.body.price,
-                    image_product: imageProduct,
-                    }, {
-                        where: {id:req.params.id}
-                    })               
-            }        
-                /*if(imageProduct){
-                    
+                let errors = validationResult(req);
+                if(!errors.isEmpty()){ 
+                    return res.render('products/editProduct', {product, category, mensajeError: errors.mapped(), old: req.body})                
+                }
+                else if(req.files){  
+                    let imageProduct;
+                    const objImages= req.files.productImage;
+                    imageProduct= Date.now() + path.extname(objImages.name);
+                    objImages.mv(__dirname+'../../../public/imagenes/productImages/'+imageProduct,(err)=>{
+                        if (err) {
+                            // aqui deberia redirigir a la pagina de error
+                            return res.send("Hubo un error");
+                        }
+                    });
                     db.product.update({
+                        name: req.body.name,
+                        id_category: req.body.category,
+                        description: req.body.description,
+                        stock: req.body.stock,
+                        price: req.body.price,
+                        image_product: imageProduct,
+                        }, {
+                            where: {id:req.params.id}
+                        })               
+                }      
+                else if (req.body.deleteImage) {
+                    db.product.update({ 
+                        name: req.body.name,
+                        id_category: req.body.category,
+                        description: req.body.description,
+                        stock: req.body.stock,
+                        price: req.body.price,
+                        image_product: "",
+                        },
+                        {
+                            where: {id:req.params.id}
+                        })
+                }
+                else  {
+                    db.product.update({ 
                     name: req.body.name,
                     id_category: req.body.category,
                     description: req.body.description,
                     stock: req.body.stock,
                     price: req.body.price,
-                    image_product: imageProduct,
-                    }, {
-                        where: {id:req.params.id}
-                    })     
-                        
-                }*/        
-            else if (req.body.deleteImage) {
-                db.product.update({ 
-                    name: req.body.name,
-                    id_category: req.body.category,
-                    description: req.body.description,
-                    stock: req.body.stock,
-                    price: req.body.price,
-                    image_product: "",
                     },
                     {
                         where: {id:req.params.id}
-                    })
-            }
-            else  {
-                db.product.update({ 
-                name: req.body.name,
-                id_category: req.body.category,
-                description: req.body.description,
-                stock: req.body.stock,
-                price: req.body.price,
-                },
-                {
-                    where: {id:req.params.id}
-                })          
-            }
-            res.redirect('/product')
-            })
-        //})
+                    })          
+                }
+                res.redirect('/product')
+                })
     } 
 }
 
