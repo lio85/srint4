4229690878c607324642639532f866e9492839e5
lio85 <lionelprats2021@gmail.com>
2021-09-01 01:52:42 -0300

    mejoras

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -110,74 +110,66 @@ let userController = {
             })       
     },
     update: function(req,res){
-        let errors = validationResult(req);
-        if(!errors.isEmpty()){
-            db.user.findByPk(req.params.id)
-            .then(function(user){
-                return res.render('users/editProfile', {user, mensajeError: errors.array(), old:req.body})
-            })     
-        }
-        else {
-            if(req.files){
-                db.user.findByPk(req.params.id)
-                .then(function(user){
-                    if(user.user_image!=""){
-                        console.log("Esta foto se va a eliminar: "+ user.user_image);
-                        let oldImageRoute= path.join(__dirname+'../../../public/imagenes/userImages/'+user.user_image);
-                        console.log("Esta es su ubicacion: "+ oldImageRoute);
-                        fs.unlinkSync(oldImageRoute); 
-                    }
-                    const objImages= req.files.userImage;
-                    let new_image= Date.now() + path.extname(objImages.name);
-                    objImages.mv(__dirname+'../../../public/imagenes/userImages/'+new_image,(err)=>{
-                    console.log("Se creó la "+ new_image);
-                    if (err) {
-                        // aqui deberia redirigir a la pagina de error
-                        return res.send("Hubo un error");
-                    }
-                    db.user.update({
-                        user_name: req.body.user,
-                        lastName_user: req.body.lastNameUser,
-                        user_image: new_image             
-                    },{
+        let product = db.product.findByPk(req.params.id);
+        let category = db.category.findAll(); 
+        Promise.all([product, category])
+            .then(function([product, category]){    
+            let errors = validationResult(req);
+            if(!errors.isEmpty()){ 
+                return res.render('products/editProduct', {product, category, mensajeError: errors.mapped(), old: req.body})                
+            }
+            else {  
+                let imageProduct;
+                if(req.files){
+                    const objImages= req.files.productImage;
+                    imageProduct= Date.now() + path.extname(objImages.name);
+                    objImages.mv(__dirname+'../../../public/imagenes/productImages/'+imageProduct,(err)=>{
+                        if (err) {
+                            // aqui deberia redirigir a la pagina de error
+                            return res.send("Hubo un error");
+                        }
+                    });              
+                }        
+                if(imageProduct){
+                    db.product.update({
+                    name: req.body.name,
+                    id_category: req.body.category,
+                    description: req.body.description,
+                    stock: req.body.stock,
+                    price: req.body.price,
+                    image_product: imageProduct,
+                    }, {
                         where: {id:req.params.id}
-                    })
-                    req.session.destroy()
-                    return res.redirect('/users/login'); 
-                    })     
-                });                        
+                    })            
+                }        
+                else if (req.body.deleteImage) {
+                    db.product.update({ 
+                        name: req.body.name,
+                        id_category: req.body.category,
+                        description: req.body.description,
+                        stock: req.body.stock,
+                        price: req.body.price,
+                        image_product: "",
+                        },
+                        {
+                            where: {id:req.params.id}
+                        })
+                }
+                else  {
+                    db.product.update({ 
+                    name: req.body.name,
+                    id_category: req.body.category,
+                    description: req.body.description,
+                    stock: req.body.stock,
+                    price: req.body.price,
+                    },
+                    {
+                        where: {id:req.params.id}
+                    })          
+                }
+                res.redirect('/product')
             }
-            else if (req.body.deleteImage) {
-                db.user.findByPk(req.params.id)
-                    .then(function(user){
-                        console.log("Esta foto se va a eliminar: "+ user.user_image);
-                        let oldImageRoute= path.join(__dirname+'../../../public/imagenes/userImages/'+user.user_image);
-                        console.log("Esta es su ubicacion: "+ oldImageRoute);
-                        fs.unlinkSync(oldImageRoute); 
-                        db.user.update({ 
-                            user_name: req.body.user,
-                            lastName_user: req.body.lastNameUser,
-                            user_image: "",
-                            },
-                            {
-                                where: {id:req.params.id}
-                            })
-                        req.session.destroy()
-                        return res.redirect('/users/login');
-                    })       
-            }
-            else  {
-                db.user.update({ 
-                    user_name: req.body.user,
-                    lastName_user: req.body.lastNameUser,
-                },
-                {
-                    where: {id:req.params.id}
-                })           
-                req.session.destroy()
-                return res.redirect('/users/login');
-            }       
-        }    
+        })
     }    
 }
 
