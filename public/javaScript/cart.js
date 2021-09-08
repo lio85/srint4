8761301259ed6e79d1f963124710e83ac0c1bbc4
lio85 <lionelprats2021@gmail.com>
2021-09-08 01:19:36 -0300

    pruebas de como capturar informacion en cart.js

--- public/javaScript/cart.js
+++ public/javaScript/cart.js
@@ -1,3 +1,5 @@
-let url_path= window.location.pathname;
-let id_product= url_path.slice(16);
+let id_product= window.location.pathname.slice(16);
 console.log("ID del producto: " + id_product);
+
+let path_image= document.querySelector(".selected_product").attributes[1].value;
+console.log("ruta de la imagen en el proyecto: "+ path_image);
