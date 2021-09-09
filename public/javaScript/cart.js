f32aceb372db296825b926cdfcdbbe24af4d6c77
lio85 <lionelprats2021@gmail.com>
2021-09-09 03:17:29 -0300

    avanzando con el carrito

--- public/javaScript/cart.js
+++ public/javaScript/cart.js
@@ -30,12 +30,17 @@ let nombre_producto_seleccionado= document.querySelector(".nombre-producto-selec
 
 if(localStorageProducts!=null){
     for (let i=0;i<localStorageProducts.length;i++){
+        generalContainer.innerHTML+=
+        "<div class='producto-seleccionado'><label class='nombre-producto-seleccionado'><strong>Seleccionaste:</strong> "+localStorageProducts[i].nameProduct+"</label><span class='contenedor-imagen-producto-seleccionado'><img class='imagen-producto-seleccionado' src='"+localStorageProducts[i].pathImageProduct+"' alt='maceta-terracota-bemba'></span><div class='detalle-compra-padre'><div class='detalle-compra-hijo'><label>Precio unitario:</label><label class='precio-producto-seleccionado'>$"+localStorageProducts[i].priceProduct+"</label></div></div>    </div>"
+
         //nombre_producto_seleccionado.innerHTML="<strong>Seleccionaste: </strong>"+localStorageProducts[i].nameProduct;
+        /*
         generalContainer.innerHTML+="<ul class='productContainer' style='border: solid black 1px'>"
             +"<li>"+localStorageProducts[i].nameProduct+"</li>"
             +"<li><img src='" +localStorageProducts[i].pathImageProduct+"' alt='"+localStorageProducts[i].nameProduct+"'></li>"
             +"<li>$"+localStorageProducts[i].priceProduct+"</li>" 
             +"</ul>"
+        */    
     }
 }
 else {
